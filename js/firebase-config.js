80f442d20505b30486bb7df5da966725b0946403
Diana-NeoDuck <diana@duckconsultingcr.com>
2025-08-28 22:34:35 -0600

    Actualización de configuración de Firebase con datos del proyecto

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "cashtrack-app.firebaseapp.com",
-  projectId: "cashtrack-app",
-  storageBucket: "cashtrack-app.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  databaseURL: "https://cashtrack-app-default-rtdb.firebaseio.com"
+  apiKey: "", // No hay clave de API web para este proyecto
+  authDomain: "cashtrack-864b9.firebaseapp.com",
+  projectId: "cashtrack-864b9",
+  storageBucket: "cashtrack-864b9.appspot.com",
+  messagingSenderId: "855575147795",
+  appId: "cashtrack-864b9",
+  databaseURL: "https://cashtrack-864b9-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
